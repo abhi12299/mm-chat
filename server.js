06e66ddb57c257dafd8ac0b440b3bbd2420008e0
Abhishek M <abhishek.m@so.city>
2020-01-19 18:03:04 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hey');
+});
+
+app.listen(3000, () => {
+    console.log(`> http://localhost:3000`);
+});
+

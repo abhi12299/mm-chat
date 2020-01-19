d206ceaad9d02624d16044591a347ebc49f16ddc
Abhishek M <abhishek.m@so.city>
2020-01-19 19:43:37 +0530

    done part 1

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 
 const app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-    res.send('hey');
+    res.sendFile(`${__dirname}/index.html`);
 });
 
-app.listen(3000, () => {
-    console.log(`> http://localhost:3000`);
+io.on('connection', socket => {
+    socket.on('disconnect', () => {
+        console.log(`${socket.name} disconnected!`);
+        io.emit('leave', socket.name);
+    });
+
+    socket.on('join', name => {
+        console.log(`${name} joined!`);
+        socket.name = name;
+        io.emit('join', name);
+    });
+
+    socket.on('chat', data => {
+        console.log(`chat data: ${JSON.stringify(data, null, 2)}`);
+        io.emit('chat', data);
+    });
 });
 
+http.listen(3000, () => {
+    console.log('> http://localhost:3000');
+});

0d74dba2f71ab2ebb079ba5035e0af3b4cf1708c
Abhishek M <abhishek.m@so.city>
2020-02-08 00:43:20 +0530

    part 2 done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Source code for (Building a multimedia chat server using Express and Socket.io)[]
 
-Code for each part of the series is available in the corresponding branch name. E.g. for part 1, switch to branch `part-1`
+Code for each part of the series is available in the corresponding branch name. E.g. for part 2, switch to branch `part-2`
+
+To run this, make a folder named `uploads/` at the root of the project.
--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
+const Busboy = require('busboy');
 const app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
+});
+
+app.use(express.static('uploads'))
+
+app.post('/upload', function(req, res) {
+    const busboy = new Busboy({ headers: req.headers });
+    req.pipe(busboy);
+    busboy.on('file', (fieldname, file, filename) => {
+        const ext = path.extname(filename);
+        const newFilename = `${Date.now()}${ext}`;
+        req.newFilename = newFilename;
+        req.originalFilename = filename;
+        const saveTo = path.join('uploads', newFilename);
+        file.pipe(fs.createWriteStream(saveTo));
+    });
+    busboy.on('finish', () => {
+        res.json({
+            originalFilename: req.originalFilename,
+            newFilename: req.newFilename
+        });
+    });
 });
 
 io.on('connection', socket => {
@@ -35,6 +59,11 @@ io.on('connection', socket => {
         console.log(`${name} stopped typing`);
         socket.broadcast.emit('stop-type', name);
         cb();
+    });
+
+    socket.on('file', f => {
+        console.log(`File by: ${f.name}`);
+        io.emit('file', f);
     });
 });
 
